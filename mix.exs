ed77f39106495b66717ff34fade2e7ede5050c80
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-10-16 16:41:18 +0300

    initial commit

---
+++ mix.exs
@@ -0,0 +1,22 @@
+defmodule Issues.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :issues,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  def application do
+    [ applications: [ :logger, :httpoison ] ]
+  end
+
+  defp deps do
+    [
+      {:httpoison, "~> 0.9"}
+    ]
+  end
+end
