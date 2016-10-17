e1b48ae842cb4e201574d57c2347e6c47d4972d1
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-10-17 11:27:02 +0300

    executable

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # Issues
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add `issues` to your list of dependencies in `mix.exs`:
-
-    ```elixir
-    def deps do
-      [{:issues, "~> 0.1.0"}]
-    end
-    ```
-
-  2. Ensure `issues` is started before your application:
-
-    ```elixir
-    def application do
-      [applications: [:issues]]
-    end
-    ```
-
+Github issues fetcher from an excellent Dave Thomas's Programming Elixir book.
--- mix.exs
+++ mix.exs
@@ -2,16 +2,21 @@ defmodule Issues.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :issues,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app:             :issues,
+      escript:         escript_config,
+      version:         "0.1.0",
+      elixir:          "~> 1.3",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps:            deps()
+    ]
   end
 
   def application do
-    [ applications: [ :logger, :httpoison ] ]
+    [
+      applications: [ :logger, :httpoison ]
+    ]
   end
 
   defp deps do
@@ -19,5 +24,9 @@ defmodule Issues.Mixfile do
       {:httpoison, "~> 0.9"},
       {:poison, "~> 3.0"}
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
